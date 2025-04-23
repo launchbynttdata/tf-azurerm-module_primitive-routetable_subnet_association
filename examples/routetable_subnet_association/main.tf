273d0959fa810e2008e00bf0faaae7db508f1686
Andrew Missel <andrew.missel@nttdata.com>
2025-04-23 12:09:21 -0500

    update to use new version of module_collection/virtual_network

--- examples/routetable_subnet_association/main.tf
+++ examples/routetable_subnet_association/main.tf
@@ -52,7 +52,7 @@ module "route_table" {
 
 module "network" {
   source  = "terraform.registry.launch.nttdata.com/module_collection/virtual_network/azurerm"
-  version = "~> 1.0"
+  version = "~> 1.1.0"
 
   network_map = local.modified_network_map
 
@@ -62,8 +62,8 @@ module "network" {
 module "rttbl_subnet_association" {
   source = "../.."
 
-  for_each = module.network.vnet_subnets
+  for_each = keys(module.network.vnet_subnets)
 
   route_table_id = module.route_table.id
-  subnet_id      = each.value[0]
+  subnet_id      = each.value
 }
